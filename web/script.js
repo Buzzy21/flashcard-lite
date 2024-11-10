07fbb1736f63955b6c69ad37ae60211d685bd12f
William Liu <lursteddev@gmail.com>
2024-11-10 15:07:49 -0500

    Introduced abstraction, editing, and shortcuts

--- web/script.js
+++ web/script.js
@@ -5,43 +5,73 @@ const revealBackButton = document.querySelector('#revealBackButton');
 const front = document.querySelector('.front');
 const back = document.querySelector('.back');
 
-
 let fronts = ["Example Front"];
 let backs = ["Example Back"]
 let idx = 0;
 let revealingFront = false;
 let revealingBack = false;
 
+//TODO: Saving edits
+
 function update() {
     // Front
     if(revealingFront) {
+        // Set front content and make it editable
         front.textContent = fronts[idx];
-        front.contenteditable = true;
+        front.contentEditable = true;
     }
     else {
+        // Hide front content and make it uneditable
         front.innerHTML = "<h3>Hidden</h3>";
-        front.contenteditable = false;
+        front.contentEditable = false;
     }
 
     // Back
     if(revealingBack) {
+        // Set back content and make it editable
         back.textContent = backs[idx];
-        back.contenteditable = true;
+        back.contentEditable = true;
     }
     else {
+        // Hide back content and make it uneditable
         back.innerHTML = "<h3>Hidden</h3>";
-        back.contenteditable = false;
+        back.contentEditable = false;
     }
 }
 
-revealFrontButton.addEventListener('click', function() {
-    revealingFront = !revealingFront;
+function toggle(swapFront) {
+    // Swap the revealing modes
+    if(swapFront) {
+        revealingFront = !revealingFront;
+    }
+    else {
+        revealingBack = !revealingBack;
+    }
     update();
+}
+
+document.addEventListener("keydown", function(event) {
+    // Prevent accidental shortcut usages when actually typing
+    if (document.activeElement.tagName == 'INPUT' ||
+        document.activeElement.tagName == 'TEXTAREA' ||
+        document.activeElement.isContentEditable) {
+        return;
+    }
+
+    if(event.key == 'l') {
+        toggle(false);
+    }
+    if(event.key == 'k') {
+        toggle(true);
+    }
+})
+
+revealFrontButton.addEventListener('click', function() {
+    toggle(true);
 })
 
 revealBackButton.addEventListener('click', function() {
-    revealingBack = !revealingBack;
-    update();
+    toggle(false)
 })
 
 nextButton.addEventListener('click', function() {
